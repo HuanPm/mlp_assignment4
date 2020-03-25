75f2cc4c20f0732320b797131b9222c7d187c06e
tinozxc123 <tinozxc123@gmail.com>
2020-03-25 18:28:27 +0800

    add plot graph functions

--- main.py
+++ main.py
@@ -54,20 +54,82 @@ def plot_result_graphs(plot_name, enn_stats, pnn_stats):
         orientation='portrait', papertype=None, format='pdf',
         transparent=False, bbox_inches=None, pad_inches=0.1,
         frameon=None, metadata=None)
+    
+    
+def plot_pnn_graphs(plot_name, pnn_stats):
+    fig_1 = plt.figure(figsize=(8, 4))
+    ax_1 = fig_1.add_subplot(111)    
+    
+    ax_1.plot(np.arange(0, len(pnn_stats['train_loss'])), 
+                       pnn_stats['train_loss'], label='pnn_train_loss')
+    ax_1.plot(np.arange(0, len(pnn_stats['val_loss'])), 
+                        pnn_stats['val_loss'], label='pnn_val_loss')
+    
+    ax_1.legend(loc=0)        
+    ax_1.set_ylabel('Loss')
+    ax_1.set_xlabel('Epoch number')
+    ax_1.grid(False)
+
+
+    # Plot the change in the validation and training set accuracy over training.
+    fig_2 = plt.figure(figsize=(8, 4))
+    ax_2 = fig_2.add_subplot(111)
+    
+    ax_2.plot(np.arange(0, len(pnn_stats['train_acc'])), 
+              pnn_stats['train_acc'], label='pnn_train_acc')
+    ax_2.plot(np.arange(0, len(pnn_stats['val_loss'])), 
+              pnn_stats['val_acc'], label='pnn_val_acc')
+                    
+    ax_2.legend(loc=0)
+    ax_2.set_ylabel('Accuracy')
+    ax_2.set_xlabel('Epoch number')
+    
+    fig_1.savefig('./data/{}_loss_performance.pdf'.format(plot_name), dpi=None, facecolor='w', edgecolor='w',
+        orientation='portrait', papertype=None, format='pdf',
+        transparent=False, bbox_inches=None, pad_inches=0.1,
+        frameon=None, metadata=None)
+    
+    fig_2.savefig('./data/{}_accuracy_performance.pdf'.format(plot_name), dpi=None, facecolor='w', edgecolor='w',
+        orientation='portrait', papertype=None, format='pdf',
+        transparent=False, bbox_inches=None, pad_inches=0.1,
+        frameon=None, metadata=None)
+    
+    
+def plot_acc_recall_graphs(plot_name, accs, recalls):    
+    fig = plt.figure(figsize=(8, 4))
+    ax = fig.add_subplot(111)
+    ax.plot(np.arange(0, accs.shape[0]), accs, label='accuracy')
+    ax.plot(np.arange(0, recalls.shape[0]), recalls, '--', label='recall')
+    
+    ax.legend(loc=0)
+    ax.set_ylabel('accuracy & recall')
+    ax.set_xlabel(plot_name)
+    
+    fig.savefig('./data/{}_acc_recall.pdf'.format(plot_name), dpi=None, facecolor='w', edgecolor='w',
+        orientation='portrait', papertype=None, format='pdf',
+        transparent=False, bbox_inches=None, pad_inches=0.1,
+        frameon=None, metadata=None)
 
 if __name__ == '__main__':
+    experiment_name = 'test'
+    
     batch_size = 100
     num_epochs = 3
     weight_decay_coefficient = 0
+    lr = 0.001
     use_gpu = True
+    
+    use_enn = False
     
     enn_input_dim = 52 + 2 + 318
     enn_output_dim = 52
     enn_hidden_dim = 1500
     enn_layers = 8
     
-    pnn_input_dim = enn_input_dim + enn_output_dim
-    #pnn_input_dim = enn_input_dim
+    if use_enn:
+        pnn_input_dim = enn_input_dim + enn_output_dim
+    else:
+        pnn_input_dim = enn_input_dim
     pnn_output_dim = 38
     pnn_hidden_dim = 1200
     pnn_layers = 10
@@ -80,11 +142,14 @@ if __name__ == '__main__':
     val_data_loader = DataLoader(val_data, batch_size=batch_size, shuffle=True, num_workers=4)
     test_data_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True, num_workers=4)
     
-    enn = ENN(
-            input_shape=(batch_size, enn_input_dim),
-            num_output_classes=enn_output_dim,
-            num_filters=enn_hidden_dim,
-            num_layers=enn_layers)
+    if use_enn:
+        enn = ENN(
+                input_shape=(batch_size, enn_input_dim),
+                num_output_classes=enn_output_dim,
+                num_filters=enn_hidden_dim,
+                num_layers=enn_layers)
+    else:
+        enn = None
     
     pnn = PNN(
             input_shape=(batch_size, pnn_input_dim),
@@ -98,8 +163,19 @@ if __name__ == '__main__':
                                         weight_decay_coefficient=weight_decay_coefficient,
                                         use_gpu=use_gpu,
                                         train_data=train_data_loader, val_data=val_data_loader,
-                                        test_data=test_data_loader)  # build an experiment object
+                                        test_data=test_data_loader,
+                                        lr=lr)  # build an experiment object
     
-    enn_stats, pnn_stats = bridge_experiment.run_experiment()
+    if use_enn:
+        enn_stats, pnn_stats = bridge_experiment.run_experiment()
+        plot_result_graphs('baseline', enn_stats, pnn_stats)
+    else:
+        pnn_stats = bridge_experiment.run_experiment()
+        plot_pnn_graphs('only_pnn', pnn_stats)
     
-    plot_result_graphs('baseline', enn_stats, pnn_stats)
+    if use_enn:
+        enn_accs, enn_recalls = bridge_experiment.eval_ordered_card()
+        plot_acc_recall_graphs('ordered_card', enn_accs, enn_recalls)
+    
+    pnn_accs, pnn_recalls = bridge_experiment.eval_ordered_bid()
+    plot_acc_recall_graphs('ordered_bid', pnn_accs, pnn_recalls)
